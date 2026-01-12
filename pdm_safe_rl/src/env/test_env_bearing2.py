be9c923ce91b9502cade994d4e690020a38155f4
kaboo4sure <kaboo4real@gmail.com>
2026-01-11 18:50:32 -0600

    added testing script

---
+++ pdm_safe_rl/src/env/test_env_bearing2.py
@@ -0,0 +1,12 @@
+from pdm_safe_rl.src.env.maintenance_NASABearing_env import NASABearingMaintenanceEnv
+
+env = NASABearingMaintenanceEnv()
+obs, info = env.reset()
+print("obs shape:", obs.shape)
+print("info:", info)
+
+for i in range(5):
+    obs, r, term, trunc, info = env.step(0)  # do nothing
+    print(i, r, term, info["t"], info["true_rul"], info["mu_rul"], info["sigma_rul"], info["p_unsafe"])
+    if term or trunc:
+        break
